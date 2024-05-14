a78c37e849264783251002e9002cb95cd459b823
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-05-14 16:11:43 +0530

    finished creating the Navbar Component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,8 +17,8 @@ const Navbar = () => {
           window.scrollTo(0,0);
         }}>
           <img src={logo} alt={logo} className='w-9 h-9 object-contain' />
-        <p className='text-white text-[18px] font-bold cursor-pointer'>
-          GeekyyFit <span className='sm:block hidden'>| Gaurav Rajput</span>
+        <p className='text-white text-[18px] font-bold cursor-pointer flex'>
+          GeekyyFit &nbsp;<span className='sm:block hidden'> | Gaurav Rajput</span>
         </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
@@ -36,11 +36,11 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        
+
         <div className='sm:hidden flex flex-1 justify-end items-center'>
           <img src={toggle ? close : menu} alt={menu} className='w-[28px] h-[28px] object-contain cursor-pointer' onClick={() =>setToggle(!toggle)}/>
           <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
-          
+
           <ul className='list-none flex justify-end items-start flex-col gap-4 '>
           {navLinks.map((link)=>(
             <li
@@ -50,7 +50,7 @@ const Navbar = () => {
               ? "text-white"
               : "text-secondary"
             }font-poppins font-medium cursor-pointer text-[16px]`}
-            onClick={()=> { 
+            onClick={()=> {
               setToggle(!toggle);
               setActive(link.title);
             }}
