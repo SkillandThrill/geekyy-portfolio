87452c3bd283f40ec6321da1a78e7e3cbdde0e7a
Gauravsing J. Rajput <127656660+GauravR02@users.noreply.github.com>
2024-05-20 14:42:51 +0530

    started creating the Tech section of the 3D balls which will look good on the website..to get things start of we are importing BallCanvas, to develope these balls using threeJS and react Three Fiber..replicated the canvas component from Computer.jsx

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React,{Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import {
+  Decal, Float, Preload, OrbitControls, useTexture
+} from '@react-three/drei'
 
-const Ball = () => {
+import CanvasLoader from '../Loader'
+
+
+
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl])
   return (
     <div>Ball</div>
+  )
+}
+
+const BallCanvas = ({icon}) =>{
+  return(
+    <Canvas>
+      
+    </Canvas>
   )
 }
 
